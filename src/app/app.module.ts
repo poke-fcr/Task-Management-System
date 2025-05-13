9fe72d635db2040b3c43701d6fc90f0ad3ee455c
poke-fcr <poke.fcr@gmail.com>
2025-05-14 01:50:09 +0530

    integrated firebase and hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,15 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 import { HttpClientModule } from '@angular/common/http';
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCyLfhuq4i0VcncsyBrLGDKDTfDxMeCPj8",
+  authDomain: "task-management-system-2.firebaseapp.com",
+  projectId: "task-management-system-2",
+  storageBucket: "task-management-system-2.firebasestorage.app",
+  messagingSenderId: "52080322466",
+  appId: "1:52080322466:web:d68d7851b99b7aa865b4fb"
+};
 
 @NgModule({
   declarations: [
@@ -32,3 +41,5 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const app = initializeApp(firebaseConfig);
